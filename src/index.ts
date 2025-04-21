b0cc4f658fa850c438fbc6f1a259e273770aef63
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-04-21 10:44:11 +0530

    Adding raw metrics

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import express from "express";
+import { middleware } from "./middleware";
 
 const app = express();
 const PORT = 3000;
 
+app.use(middleware);
+
 app.get("/", (req, res) => {
   res.send("Hello from Bun + Express!");
+});
+
+app.get("/user", (req, res) => {
+  res.send({
+    name: "Manoj Kumar",
+    age: 25,
+  });
+});
+
+app.post("/user", (req, res) => {
+  const user = req.body;
+  res.send({
+    ...user,
+    id: Math.floor(Math.random() * 1000),
+  });
 });
 
 app.listen(PORT, () => {
